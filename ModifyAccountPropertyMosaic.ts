eaf152e007fdd4a181e7557a8d3715a62ef927c5
MIJ <mijinco1024@gmail.com>
2019-06-19 20:35:30 +0900

    update

--- ModifyAccountPropertyMosaic.ts
+++ ModifyAccountPropertyMosaic.ts
@@ -1,16 +1,9 @@
 // ** USAGE **
 //    Transaction Blocking Property
 //
-//    $ ts-node ModifyAccountPropertyEntity.ts <private key> <AllowTransaction|BlockTransaction> <Add|Remove> <TransationType>
+//    $ ts-node ModifyAccountPropertyEntity.ts <private key> <AllowMocaic|BlockMosaic> <Add|Remove> <MosaicId( 8byte-hex )>
 //
 //    ( All arguments (4 args) are required.)
-//
-//    TransactionType
-//        'TRANSFER', 'REGISTER_NAMESPACE', 'ADDRESS_ALIAS', 'MOSAIC_ALIAS',
-//        'MOSAIC_DEFINITION', 'MOSAIC_SUPPLY_CHANGE', 'MODIFY_MULTISIG_ACCOUNT',
-//        'AGGREGATE_COMPLETE', 'AGGREGATE_BONDED', 'LOCK', 'SECRET_LOCK', 'SECRET_PROOF',
-//        'MODIFY_ACCOUNT_PROPERTY_ADDRESS', 'MODIFY_ACCOUNT_PROPERTY_MOSAIC',
-//        'MODIFY_ACCOUNT_PROPERTY_ENTITY_TYPE', 'LINK_ACCOUNT'
 //
 
 import * as nem from 'nem2-sdk';
@@ -27,7 +20,7 @@ const url = 'http://localhost:3000';
 const optParse = new DefaultOptParse();
 optParse.subscribe(
     'propertyType',
-    (arg: string) => { return arg === 'AllowTransaction' || arg === 'BlockTransaction' },
+    (arg: string) => { return arg === 'AllowMosaic' || arg === 'BlockMosaic' },
     (arg: string) => { return `${(<any>nem.PropertyType)[arg]}` }
 );
 optParse.subscribe(
@@ -36,31 +29,31 @@ optParse.subscribe(
     (arg: string) => { return `${(<any>nem.PropertyModificationType)[arg]}` }
 );
 optParse.subscribe(
-    'entityType',
-    (arg: string) => { return Object.keys(nem.TransactionType).includes(arg) },
-    (arg: string) => { return `${nem.TransactionType[<keyof nem.TransactionType>arg]}` }
+    'mosaicId',
+    (arg: string) => { return Util.isHex(arg) && arg.length === 16 }
 );
 const option = optParse.parse();
 
 const privateKey = option.get('privateKey');
 const propertyType = option.get('propertyType');
 const modificationType = option.get('modificationType');
-const entityType = option.get('entityType');
+const hexMosaicId = option.get('mosaicId');
 
 // argument check
-[privateKey, propertyType, modificationType, entityType].forEach(arg => {
+[privateKey, propertyType, modificationType, hexMosaicId].forEach(arg => {
     if (Util.isUndefined(arg)) {
         console.error(`argument parse fault.`);
         process.exit(1);
     }
 });
 
-const modifiedAccount = nem.Account.createFromPrivateKey(privateKey, netType);
+const modifiedAccount  = nem.Account.createFromPrivateKey(privateKey, netType);
+const modifiedMosaicId = new nem.MosaicId(hexMosaicId);
 const modifications = [
-    nem.AccountPropertyModification.createForEntityType(parseInt(modificationType),parseInt(entityType) )
+    nem.AccountPropertyModification.createForMosaic(parseInt(modificationType),modifiedMosaicId )
 ];
 
-const modifyAccountPropertyAddressTx = nem.AccountPropertyTransaction.createEntityTypePropertyModificationTransaction(
+const modifyAccountPropertyAddressTx = nem.AccountPropertyTransaction.createMosaicPropertyModificationTransaction(
     nem.Deadline.create(),
     parseInt(propertyType),
     modifications,
